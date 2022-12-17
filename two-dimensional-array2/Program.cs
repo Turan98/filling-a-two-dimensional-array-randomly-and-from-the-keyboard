67fe274095e467e3686bd01aa875b356d57e84e7
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-12-17 13:18:24 +0400

    test19

---
+++ two-dimensional-array2/Program.cs
@@ -0,0 +1,26 @@
+﻿int a, b;
+Console.Write("Enter height of array: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter weight of array: ");
+b = Convert.ToInt32(Console.ReadLine());
+int [,] array1 = new int [a,b];
+//Random random = new Random();
+Console.WriteLine("Enter elements of array by position: ");
+for (int i = 0; i < array1.GetLength(0); i++)
+{
+    for (int j = 0; j < array1.GetLength(1); j++)
+    {
+        //array1[i,j] = random.Next(50);
+        Console.WriteLine("H: " + i + "\tW: " + j);
+        array1[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+Console.WriteLine();
+for (int i = 0; i < array1.GetLength(0); i++)
+{
+    for (int j = 0; j < array1.GetLength(1); j++)
+    {
+        Console.Write(array1[i,j] + "\t");
+    }
+    Console.WriteLine();
+}
